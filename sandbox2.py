1ce3436e1e3e36da84c2126eecc245f53c8286b8
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-07-18 00:00:02 +1200

    testing button bind stuff

--- sandbox2.py
+++ sandbox2.py
@@ -1,67 +1,11 @@
-from random import randint
-from tkinter import *
-from turtle import ScrolledCanvas, RawTurtle, TurtleScreen
-print("\033[1;32m This text is Bright Green  \n")
-
-
-health = 50
-damage = 10
-fight = randint(10, 20)
-step = 0
-
-def up():
-    global step
-
-    if step == fight:
-        combat()
-    step += 1
-    turtle.seth(90)
-    turtle.forward(10)
-
-def down():
-    global step
-
-    if step == fight:
-        combat()
-    step += 1
-    turtle.seth(-90)
-    turtle.forward(10)
-
-def left():
-    global step
-
-    if step == fight:
-        combat()
-    step += 1
-    turtle.seth(180)
-    turtle.forward(10)
-
-def right():
-    global step
-
-    if step == fight:
-        combat()
-    step += 1
-    turtle.seth(0)
-    turtle.forward(10)
-
-def combat():
-    enemy = RawTurtle(canvas)
-    enemy.up()
-    eHealth = randint(20, 100)
-    eDamage = randint(10, 20)
+from tkinter import * 
 
 root = Tk()
-canvas = ScrolledCanvas(root)
-canvas.pack(side=LEFT)
-screen = TurtleScreen(canvas)
-turtle = RawTurtle(canvas)
-turtle.up()
+root.geometry("600x600")
+button = Button(root, text="Click here", command=lambda: button.config(state=DISABLED))
+button.grid()
+button.bind('<Return>', lambda e: button.config(state=NORMAL))
 
-screen.onkey(up, "Up")
-screen.onkey(down, "Down")
-screen.onkey(left, "Left")
-screen.onkey(right, "Right")
-screen.listen()
 
-screen.mainloop()
+
+root.mainloop()
